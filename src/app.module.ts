f2c22c7cf1e9b77de9bc67222f02365229715e44
MubbashirHussain <mubbashirhhussain@gmail.com>
2025-11-16 14:30:07 +0500

    FIx import Error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SuperadminModule } from './modules/superadmin/superadmin.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -9,8 +8,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
-    }),
-    SuperadminModule],
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
